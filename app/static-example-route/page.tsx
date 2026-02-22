0a13ed6c25b9f26ccd966cb6488ccb6e16916d1a
peterlidee <peter@lidee.be>
2026-02-22 17:38:15 +0100

    Added static example route

---
+++ app/static-example-route/page.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/static/Header';
+import Main from '../components/static/Main';
+import Footer from '../components/static/Footer';
+
+export default function page() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
