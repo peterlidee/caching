ca7b8e74e5269556a98fb2cc3a3d952e547d17b0
peterlidee <peter@lidee.be>
2026-02-22 17:39:00 +0100

    Added dynamic example route

---
+++ app/dynamic-example-route/page.tsx
@@ -0,0 +1,17 @@
+import Header from '../components/static/Header';
+import Main from '../components/static/Main';
+import Footer from '../components/static/Footer';
+
+export default async function page({
+  searchParams,
+}: PageProps<'/dynamic-example-route'>) {
+  const resolvedSearchParams = await searchParams;
+  return (
+    <>
+      <div>Hello {resolvedSearchParams.name}</div>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
