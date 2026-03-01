7f11d6282d779ad05f080be9bb50af3d9c150ddc
peterlidee <peter@lidee.be>
2026-03-01 18:11:18 +0100

    Setup home routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
+const links = [
+  { label: 'static example route', route: '/static-example-route' },
+  {
+    label: 'dynamic example route',
+    route: '/dynamic-example-route?name=peter',
+  },
+  { label: 'static client', route: '/static-client' },
+];
+
 export default function Home() {
-  return <>home</>;
+  return (
+    <>
+      <h1 className='font-bold'>home</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.route}>
+            <Link href={link.route} className='underline text-blue-500'>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
