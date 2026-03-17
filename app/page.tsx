92a26854dae25c4e85b0b4b821272b6a40827920
peterlidee <peter@lidee.be>
2026-03-17 21:17:26 +0100

    Updated links with chapters

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,56 @@
 import Link from 'next/link';
 
-const links = [
-  { label: 'static example route', route: '/static-example-route' },
+const chapters = [
   {
-    label: 'dynamic example route',
-    route: '/dynamic-example-route?name=peter',
+    title: 'Chapter 2: static and dynamic routes',
+    links: [
+      { label: 'static example route', route: '/static-example-route' },
+      {
+        label: 'dynamic example route',
+        route: '/dynamic-example-route?name=peter',
+      },
+    ],
   },
-  { label: 'static client', route: '/static-client' },
-  { label: 'ISR blog', route: '/blog' },
+  {
+    title: 'Chapter 3: client components',
+    links: [{ label: 'static client', route: '/static-client' }],
+  },
+  {
+    title: 'Chapter 5: time based revaldiation',
+    links: [{ label: 'revalidate time', route: '/revalidateTime' }],
+  },
+  {
+    title: 'Chapter 6: revalidatePath',
+    links: [{ label: 'revalidate path tests', route: 'revalidatePath' }],
+  },
+  {
+    title: 'Chapter 8: ISR',
+    links: [{ label: 'ISR blog', route: '/blog' }],
+  },
 ];
 
 export default function Home() {
   return (
     <>
-      <h1 className='font-bold'>home</h1>
-      <ul>
-        {links.map((link) => (
-          <li key={link.route}>
-            <Link href={link.route} className='underline text-blue-500'>
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1 className='text-xl font-bold mb-4'>home</h1>
+      <div>
+        {chapters.map((chapter) => {
+          return (
+            <div key={chapter.title} className='mb-4'>
+              <h2>{chapter.title}</h2>
+              <ul>
+                {chapter.links.map((link) => (
+                  <li key={link.route}>
+                    <Link href={link.route} className='underline text-blue-500'>
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
