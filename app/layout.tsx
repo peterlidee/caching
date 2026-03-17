bf0fe6b50c53d963fc294064a7bac04be3399c7a
peterlidee <peter@lidee.be>
2026-03-17 21:17:16 +0100

    Added home link to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,6 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}
       >
+        <aside>
+          <Link href='/' className='underline text-blue-500 block mb-4'>
+            &lt; home
+          </Link>
+        </aside>
         {children}
       </body>
     </html>
