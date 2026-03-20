f1df25711cebf77476b77547cfd175d8afdd8aeb
peterlidee <peter@lidee.be>
2026-03-20 17:40:19 +0100

    Updated example with date

--- components/Post.tsx
+++ components/Post.tsx
@@ -14,9 +14,14 @@ export default async function Post({ postId, fetchOptions }: Props) {
     fetchOptions,
   );
   const post: PostT = await data.json();
+  // get date from headers
+  const fetchDate = data.headers.get('date');
+  const time = fetchDate ? new Date(fetchDate).toLocaleTimeString() : null;
   return (
-    <div>
-      {post.id}. {post.title}
+    <div className='flex gap-2'>
+      <div className='text-orange-400 mr-2'>{time}</div>
+      <div className='font-bold'>{post.id}.</div>
+      <div className='italic'>{post.title}</div>
     </div>
   );
 }
