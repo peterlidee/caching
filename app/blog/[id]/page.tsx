c514618af65024002beab9d506231d3d939c51c9
peterlidee <peter@lidee.be>
2026-03-26 21:24:45 +0100

    Updated to SinglePost component

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,18 +1,14 @@
-import Post from '@/components/blog/Post';
 import { getPosts } from '../page';
+import SinglePost from '@/components/blog/SinglePost';
 
-export async function generateStaticParams2() {
+export async function generateStaticParams() {
   const posts = await getPosts();
   return posts.map((post) => ({ id: post.id.toString() }));
-}
-// TODO: put this back
-export async function generateStaticParams() {
-  return [];
 }
 
 export const dynamicParams = true; // true | false
 
 export default async function page({ params }: PageProps<'/blog/[id]'>) {
   const { id } = await params;
-  return <Post id={id} />;
+  return <SinglePost id={id} />;
 }
